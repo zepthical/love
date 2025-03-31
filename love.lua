56e9e8157c81d6e418fe4b5774765c65efeee856
Zepth <taitanerenwearenumber@gmail.com>
2025-03-31 17:19:58 +0700

    Update and rename README.md to love.lua

---
+++ love.lua
@@ -0,0 +1,58 @@
+# I love you
+
+I love your cheeks (i mean by on your face)
+I love your face
+I love your ears 
+I love your hair
+I love your confidence 
+I love your nose
+I love your hands
+I love your shoulders 
+I love your smile 
+I love that you are brave
+I love that you are kind
+I love that you are strong 
+I love that you are you
+I love that your smart
+I love that you are creative 
+You are beautiful
+You are nice
+You are confident 
+You are smart
+You are unstoppable 
+You are perfect 
+You are unique 
+You are amazing 
+You are pretty 
+I love when you are mad
+I love when you are sad
+I love when you are happy 
+I love when you are anxious
+I love when your embarrassed 
+I love when your disappointed
+I love when you are cheerful
+I love when your at your lowest 
+I love when your at your highest 
+I'm proud of you for eating 
+I'm proud of you for setting goals 
+I'm proud of you for getting up
+I'm proud of you for learning 
+I'm proud of you for trying 
+I'm proud of you smiling 
+I'm proud of you for going outside
+I'm proud of you for spreading kindness 
+I'm proud of you being you
+I'm proud of you no matter what 
+I know your insecure but everyone is
+I know your not happy but your not alone
+I know your getting bullied but don’t listen to them
+I know your sad but try being happy 
+I know your miserable but your still standing 
+I know you feel hated but your loved
+I know you feel sad but deep down your happy 
+I know you feel disappointed but you are proud 
+I know you are depressed but smile!!
+
+Insecurities are only real because of society,so ask yourself.. “would you be who you are right now if it weren’t for them”and what would you say?dont hate yourself,because you are worth the world!everyone is different thats a good thing. Dont be insecure,just set goals! be yourself and learn to love who you are >3
+
+# I really really love you
